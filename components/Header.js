8d4c7f0f5ffebd57abd612b0c47100bb18cad2ad
Rushil  Patel <rushil@rushil.local>
2022-02-23 21:20:04 -0800

    first commit

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,14 @@
+import styles from './Header.module.css'
+
 export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+
+  return (
+    <h1 className={styles.header}>
+      {
+        title.split('').map((character => {
+          return <span className={styles.fadeCharacter}>{character}</span>
+        }))
+      }
+    </h1>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+
 export default function Home() {
   return (
     <div className="container">
@@ -11,10 +12,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Rushil Patel" />
       </main>
 
       <Footer />
